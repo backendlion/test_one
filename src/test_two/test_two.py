a11a2cd4b43be31c184f68ba06dd95fc7180fdf8
Anderson F Lima <afl@lionx.com.br>
2021-11-30 19:00:25 -0300

    added test files

---
+++ src/test_two/test_two.py
@@ -0,0 +1,17 @@
+"""Second Test
+Write a function that takes as arguments two arrays of integers. 
+Your function needs to determine whether the second array is a subsequence of the first one, returning true or false;
+"""
+
+def is_subsequence(array_one, array_two):
+        # Write your code here
+        pass
+
+
+
+if __name__=="__main__":
+
+        array_one = [5, 1, 22, 25, 6, -1, 8, 10]
+        array_two = [1, 6, -1, 10]
+
+        print(is_subsequence(array_one, array_two))
